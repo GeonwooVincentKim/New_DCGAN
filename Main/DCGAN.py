f2850a10b0d425a312b012c0147dde091ca28868
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-05 19:20:59 +0900

    Added requirement variables that related to EPOCH and BATCH_SIZE

--- Main/DCGAN.py
+++ Main/DCGAN.py
@@ -15,3 +15,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from IPython.display import HTML
 import import_ipynb
+
+EPOCHS = 500
+BATCH_SIZE = 100
+USE_CUDA = torch.cuda.is_available()
+DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
+print("Current Device : ", DEVICE)
