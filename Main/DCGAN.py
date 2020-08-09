5d88e81b43089dfbf62844ab564e31d54740171e
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-09 13:38:45 +0900

    Creating details of Discriminator class.

--- Main/DCGAN.py
+++ Main/DCGAN.py
@@ -189,6 +189,25 @@ class Discriminator(nn.Module):
     def __init__(self, ngpu):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input : nc * 64 * 64
+            nn.Conv2d(
+                nc, ndf,
+                4, 2, 1, bias=False
+            ),
+            nn.LeakyReLU(0, 2, inplace=True),
+
+            # ndf * 32 * 32
+            nn.Conv2d(
+                ndf, ndf * 2,
+                4, 2, 1, bias=False
+            ),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0, 2, inplace=True),
+
+
+
+        )
 
 # def run():
 #     torch.multiprocessing.freeze_support()
