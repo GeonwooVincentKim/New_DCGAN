97008812b05b4f129653e9e815d845efab6e15ef
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-09 14:46:31 +0900

    Enumerating Real-CPU and Real_Images.

--- Main/DCGAN_Training.py
+++ Main/DCGAN_Training.py
@@ -1,1 +1,24 @@
 from Main.DCGAN import *
+
+# Training
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting Training Loop...")
+
+
+# Using for-each epoch
+for epoch in range(num_epochs):
+    # for each batch
+    for i, data in enumerate(train_loader, 0):
+        # Update 0
+        # Train with All-Real-Batch.
+        netD.zero_grad()
+        real_cpu = data[0].to(DEVICE)
+        b_size = real_cpu.size(0)
+        label = torch.full(
+            (b_size, ), real_label,
+            device=DEVICE
+        )
